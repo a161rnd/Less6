840c6a8522a9d479edfd31c6969c85cb35d3241e
Администратор <a161rnd@gmail.com>
2021-07-11 16:55:59 +0300

    commit

--- src/BoxMain.java
+++ src/BoxMain.java
@@ -17,6 +17,6 @@ public class BoxMain {
         System.out.println("Vol2 is " + mybox1.volume2());
         System.out.println("Vol2 is " + mybox2.volume2());
 
-        System.out.println("new_branch");
+        System.out.println("new_branch 333");
     }
 }
